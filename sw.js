20a8271da1b7c791fb15f3e3c56040472704de1a
gsiegel14 <siegel.gabe@gmail.com>
2025-06-19 15:32:16 -0600

    Updates

---
+++ sw.js
@@ -0,0 +1,153 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-40861e81'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "apple-touch-icon.png",
+    "revision": "aa880320c3880bfe2b34a774f8d15457"
+  }, {
+    "url": "assets/index-vpoUoAHQ.js",
+    "revision": null
+  }, {
+    "url": "denver-health-logo.svg",
+    "revision": "bd2017d9ba48da73a2ffb0bf88eaa962"
+  }, {
+    "url": "denver-pocus-logo.png",
+    "revision": "6d8a0c1854c9b01000b82824ee9216c7"
+  }, {
+    "url": "icon-192.png",
+    "revision": "bf9ad83ac9178b8c9aa284079ffff388"
+  }, {
+    "url": "icon-512.png",
+    "revision": "5de1f50d728fc1f364dad67f3ca2afb9"
+  }, {
+    "url": "index.html",
+    "revision": "692c4fc51793e41bb05c798d3745b73c"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "93e782d9861875f3e54eabf763e25d52"
+  }, {
+    "url": "registerSW.js",
+    "revision": "1872c500de691dce40960bb85481de07"
+  }, {
+    "url": "vite.svg",
+    "revision": "8e3a10e157f75ada21ab742c022d5430"
+  }, {
+    "url": "apple-touch-icon.png",
+    "revision": "aa880320c3880bfe2b34a774f8d15457"
+  }, {
+    "url": "denver-health-logo.svg",
+    "revision": "bd2017d9ba48da73a2ffb0bf88eaa962"
+  }, {
+    "url": "icon-192.png",
+    "revision": "bf9ad83ac9178b8c9aa284079ffff388"
+  }, {
+    "url": "icon-512.png",
+    "revision": "5de1f50d728fc1f364dad67f3ca2afb9"
+  }, {
+    "url": "vite.svg",
+    "revision": "8e3a10e157f75ada21ab742c022d5430"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "93e782d9861875f3e54eabf763e25d52"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+  workbox.registerRoute(/^https:\/\/fonts\.googleapis\.com\/.*/i, new workbox.CacheFirst({
+    "cacheName": "google-fonts-cache",
+    plugins: [new workbox.ExpirationPlugin({
+      maxEntries: 10,
+      maxAgeSeconds: 31536000
+    })]
+  }), 'GET');
+  workbox.registerRoute(/^https:\/\/fonts\.gstatic\.com\/.*/i, new workbox.CacheFirst({
+    "cacheName": "gstatic-fonts-cache",
+    plugins: [new workbox.ExpirationPlugin({
+      maxEntries: 10,
+      maxAgeSeconds: 31536000
+    })]
+  }), 'GET');
+  workbox.registerRoute(/^https?:\/\/.*\//, new workbox.NetworkFirst({
+    "cacheName": "pages-cache",
+    plugins: [new workbox.ExpirationPlugin({
+      maxEntries: 50,
+      maxAgeSeconds: 2592000
+    })]
+  }), 'GET');
+
+}));
